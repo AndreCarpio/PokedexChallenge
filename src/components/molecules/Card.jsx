e49f5316a70e044decc0657a8f29af5172583c9e
AndreCarpio <Andre.Carpio@jala.university>
2025-05-31 00:03:05 -0400

    adding images to pokemon card (dottedPattern, pokeballBg, PokemonImage)

--- src/components/molecules/Card.jsx
+++ src/components/molecules/Card.jsx
@@ -2,19 +2,29 @@ import { getTypeColor } from "../../utils/getTypeColor"
 import { CardNumber } from "../atoms/CardNumber"
 import { CardPokemonName } from "../atoms/CardPokemonName"
 import { PokemonTags } from "../atoms/PokemonTags"
+import dottedPattern from "../../assets/pokemonCardImages/dottedPattern.png"
+import pokeballBackground from "../../assets/pokemonCardImages/pokeballBackground.png"
 import "./Card.css"
 
 export const Card = ({ pokemonName = "Sin Nombre", pokemonNumber = "#000", imageURL = "", types = [] }) => {
     return (
         <div className="pokemonCard" style={{ backgroundColor: getTypeColor(types[0]) }} >
-            <div className=' '>
+
+            <div style={{ display: 'flex', flex: 2, flexDirection: 'column', justifyContent: 'flex-end', textAlign: "start" }} >
                 <CardNumber pokemonNumber={pokemonNumber}></CardNumber>
                 <CardPokemonName pokemonName={pokemonName}></CardPokemonName>
             </div>
 
-            <div className=' '>
+            <div style={{ display: 'flex', flex: 1, flexDirection: 'column', justifyContent: 'center' }}>
                 <PokemonTags types={types}></PokemonTags>
             </div>
+
+            <img src={dottedPattern} style={{ position: 'absolute', top: '2.1rem', height: "18%" }} />
+
+            <img src={pokeballBackground} style={{ position: 'absolute', top: 0, right: 0, height: "100%" }} />
+
+            <img src={imageURL} style={{ position: 'absolute', height: '80%', right: '-3rem' }} />
+
         </div>
     )
 }
