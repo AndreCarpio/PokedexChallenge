9e33cdac117f8fee8169d8a91130ef405f219afa
AndreCarpio <Andre.Carpio@jala.university>
2025-05-31 02:02:36 -0400

    adding tag images

--- src/components/atoms/PokemonTags.jsx
+++ src/components/atoms/PokemonTags.jsx
@@ -1,8 +1,8 @@
 
 import { capitalizeFirstLetter } from "../../utils/capitalizeFirstLatter"
 import { getTagColor } from "../../utils/getTagColor.js"
-import { getTypeColor } from "../../utils/getTypeColor"
 import "./PokemonTags.css"
+import { TypeIcon } from "./TypeIcon.jsx"
 
 export const PokemonTags = ({ types = [] }) => {
 
@@ -12,6 +12,10 @@ export const PokemonTags = ({ types = [] }) => {
                 types.map((type, index) => {
                     return (
                         <div key={index} className="tagCard" style={{ backgroundColor: getTagColor(type) }}>
+                            <div className="tagCardImage">
+                                <TypeIcon type={type}></TypeIcon>
+                            </div>
+
                             <p>{capitalizeFirstLetter(type)}</p>
                         </div>
                     )
