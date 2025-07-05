111e47c29e23de83b19e6f123081d83dd6d6c889
AndreCarpio <Andre.Carpio@jala.university>
2025-07-05 09:48:06 -0400

    game section: get data and shuffle data array

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -1,5 +1,58 @@
+import { useEffect, useState } from "react";
 import "./Game.css";
+import { DefaultButton } from "../components/atoms/DefaultButton";
+import { shuffleArrary } from "../utils/shuffleArray";
 
 export const Game = () => {
-  return <main>game</main>;
+  const [game, setGame] = useState({
+    state: false,
+    gameOver: false,
+    score: 0,
+    options: [],
+    successfulChoices: [],
+    correctOption: 0,
+  });
+
+  async function startGame() {
+    let newOptions = await getPokemons();
+  }
+
+  async function getPokemons() {
+    try {
+      let res = await fetch(
+        "https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0",
+      );
+      if (res.ok) {
+        res = await res.json();
+        res = shuffleArrary(res.results);
+        return res;
+      } else {
+        throw new Error("Error to get data");
+      }
+    } catch (error) {
+      console.log("Error to get data: " + error);
+    }
+  }
+
+  return (
+    <div>
+      <div>
+        <p>Who's That Pokémon?</p>
+        {game.state && (
+          <>
+            <p>Score</p>
+            <div>
+              <img src="" alt="" />
+            </div>
+          </>
+        )}
+        {!game.state && (
+          <>
+            <DefaultButton onCLick={getPokemons}>Start</DefaultButton>
+          </>
+        )}
+      </div>
+      <div></div>
+    </div>
+  );
 };
