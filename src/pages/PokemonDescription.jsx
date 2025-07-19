166c6804133a6817e5822d34092888cd13d9135f
AndreCarpio <Andre.Carpio@jala.university>
2025-07-19 17:31:59 -0400

    adding styles to pokemon descriptio page

--- src/pages/PokemonDescription.jsx
+++ src/pages/PokemonDescription.jsx
@@ -13,26 +13,17 @@ export const PokemonDescription = () => {
   const { pokemon, setPokemon, loading, error } = usePokemon(pokemonId);
 
   return (
-    <main>
-      {loading ? (
-        <div style={{ height: "2rem" }}>
-          <Spinner color="var(--primary-color)" />
-        </div>
-      ) : (
-        <p>fetch data</p>
-      )}
-
-      {error && <p>Error getting data</p>}
+    <main className="pokemonDescriptionContainer">
       <div className="containerInfo">
-        <ArrowbackIcon />
-        <p> {formatToHashNumber("1")}</p>
-        <p>Bulbasaur</p>
-        <p>Generation 1</p>
-        <div className="types">
+        <ArrowbackIcon size="3rem" fill="var(--gray-700)" />
+        <p className="pokemonNumber"> {formatToHashNumber("1")}</p>
+        <p className="pokemonName">Bulbasaur</p>
+        <p className="pokemonGeneration">Generation 1</p>
+        <div className="pokemonsTypes">
           <PokemonTags types={["normal", "fighting"]} />
         </div>
         <div className="navBarInformation">
-          <p className="option">Pokedex Data</p>
+          <p className="option active">Data</p>
           <p className="option">Stats</p>
           <p className="option">Evolution</p>
         </div>
@@ -44,47 +35,122 @@ export const PokemonDescription = () => {
               on its back. By soaking up the sun's rays, the seed grows
               progressively larger
             </p>
-            <div>
-              <p>Species</p>
-              <p>Seed Pokémon</p>
+            <div className="infoRow">
+              <p className="label">Species</p>
+              <p className="info">Seed Pokémon</p>
             </div>
-            <div>
-              <p>Height</p>
-              <p>0.7m (2′04″)</p>
+            <div className="infoRow">
+              <p className="label">Height</p>
+              <p className="info">0.7m (2′04″)</p>
             </div>
-            <div>
-              <p>Weight</p>
-              <p>6.9kg (15.2 lbs)</p>
+            <div className="infoRow">
+              <p className="label">Weight</p>
+              <p className="info">6.9kg (15.2 lbs)</p>
             </div>
-            <div>
-              <p>Abilities</p>
-              <p>1. Overgrow</p>
+            <div className="infoRow">
+              <p className="label">Abilities</p>
+              <p className="info">1. Overgrow</p>
             </div>
-            <div>
-              <p>Weaknesses</p>
-              <p>fire</p>
+            <div className="infoRow">
+              <p className="label">Weaknesses</p>
+              <p className="info">fire</p>
             </div>
           </div>
           <div className="stats">
             <p className="statsInfo"></p>
-            <div>
-              <p>HP</p>
+            <div className="infoRowStat">
+              <p className="stat">HP</p>
               <p className="baseStat">45</p>
               <div className="bar">
-                <div></div>
+                <div className="progress"></div>
               </div>
-              <p>min</p>
-              <p>max</p>
+              <p className="minStat">min</p>
+              <p className="maxStat">max</p>
+            </div>
+
+            <div className="infoRowStat">
+              <p className="stat">Attack</p>
+              <p className="baseStat">45</p>
+              <div className="bar">
+                <div className="progress"></div>
+              </div>
+              <p className="minStat">min</p>
+              <p className="maxStat">max</p>
+            </div>
+
+            <div className="infoRowStat">
+              <p className="stat">Defense</p>
+              <p className="baseStat">45</p>
+              <div className="bar">
+                <div className="progress"></div>
+              </div>
+              <p className="minStat">min</p>
+              <p className="maxStat">max</p>
+            </div>
+            <div className="infoRowStat">
+              <p className="stat">Sp. Atk</p>
+              <p className="baseStat">45</p>
+              <div className="bar">
+                <div className="progress"></div>
+              </div>
+              <p className="minStat">min</p>
+              <p className="maxStat">max</p>
+            </div>
+            <div className="infoRowStat">
+              <p className="stat">Sp. Def</p>
+              <p className="baseStat">45</p>
+              <div className="bar">
+                <div className="progress"></div>
+              </div>
+              <p className="minStat">min</p>
+              <p className="maxStat">max</p>
+            </div>
+            <div className="infoRowStat">
+              <p className="stat">Speed</p>
+              <p className="baseStat">45</p>
+              <div className="bar">
+                <div className="progress"></div>
+              </div>
+              <p className="minStat">min</p>
+              <p className="maxStat">max</p>
             </div>
           </div>
           <div className="evolution">
             <p className="evolutioLabel">Evolution Chart</p>
-            <div className="evolution">
-              <div>
-                <img src="" alt="" />
+            <div className="evolutionChain">
+              <div className="evolutionPokemon">
+                <div className="evolutionImageContainer">
+                  <img
+                    src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/2.png"
+                    alt=""
+                  />
+                </div>
+                <p className="pokemonNumber">#001</p>
+                <div className="pokemonName">Bulbasaur</div>
+                <div className="pokemonTypes"></div>
               </div>
-              <p>#001</p>
-              <div>types</div>
+              <div className="evolutionPokemon">
+                <div className="evolutionImageContainer">
+                  <img
+                    src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/2.png"
+                    alt=""
+                  />
+                </div>
+                <p className="pokemonNumber">#001</p>
+                <div className="pokemonName">Bulbasaur</div>
+                <div className="pokemonTypes"></div>
+              </div>
+              <div className="evolutionPokemon">
+                <div className="evolutionImageContainer">
+                  <img
+                    src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/2.png"
+                    alt=""
+                  />
+                </div>
+                <p className="pokemonNumber">#001</p>
+                <div className="pokemonName">Bulbasaur</div>
+                <div className="pokemonTypes"></div>
+              </div>
             </div>
           </div>
         </div>
