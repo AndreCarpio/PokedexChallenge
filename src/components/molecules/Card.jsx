697c39b83acc05667547b616cfe096bb9f98be65
AndreCarpio <Andre.Carpio@jala.university>
2025-07-26 12:44:45 -0400

    delete hover  and fixing reponsiveness

--- src/components/molecules/Card.jsx
+++ src/components/molecules/Card.jsx
@@ -1,11 +1,11 @@
-import { getTypeColor } from "../../utils/getTypeColor";
 import { CardNumber } from "../atoms/CardNumber";
 import { CardPokemonName } from "../atoms/CardPokemonName";
 import { PokemonTags } from "../atoms/PokemonTags";
 import dottedPattern from "../../assets/pokemonCardImages/dottedPattern.png";
 import pokeballBackground from "../../assets/pokemonCardImages/pokeballBackground.png";
-import "./Card.css";
 import { Link } from "react-router";
+import { getTypeColor } from "../../utils/getTypeColor";
+import "./Card.css";
 
 export const Card = ({ pokemon }) => {
   if (
@@ -17,55 +17,38 @@ export const Card = ({ pokemon }) => {
   ) {
     return <div className="cardError">Invalid Pokémon data</div>;
   }
+  const primaryType = pokemon.types[0].type.name;
+
   return (
     <Link
       to={`/pokemons/${pokemon.id}`}
       className="pokemonCard"
-      style={{
-        backgroundColor: getTypeColor(pokemon.types.map((t) => t.type.name)[0]),
-      }}
+      style={{ backgroundColor: getTypeColor(primaryType) }}
     >
-      <div
-        style={{
-          display: "flex",
-          flex: 2,
-          flexDirection: "column",
-          justifyContent: "flex-end",
-          textAlign: "start",
-        }}
-      >
-        <CardNumber pokemonNumber={pokemon.id}></CardNumber>
-        <CardPokemonName pokemonName={pokemon.name}></CardPokemonName>
+      <div className="cardInfo">
+        <CardNumber pokemonNumber={pokemon.id} />
+        <CardPokemonName pokemonName={pokemon.name} />
       </div>
 
-      <div
-        style={{
-          display: "flex",
-          flex: 1,
-          flexDirection: "column",
-          justifyContent: "center",
-        }}
-      >
-        <PokemonTags
-          types={pokemon.types.map((t) => t.type.name)}
-        ></PokemonTags>
+      <div className="cardTags">
+        <PokemonTags types={pokemon.types.map((t) => t.type.name)} />
       </div>
 
       <img
         src={dottedPattern}
-        style={{ position: "absolute", top: "2.1rem", height: "18%" }}
+        className="dottedPattern"
         alt="dottedPattern background"
       />
 
       <img
         src={pokeballBackground}
-        style={{ position: "absolute", top: 0, right: 0, height: "100%" }}
+        className="pokeballBackground"
         alt="pokeball background"
       />
 
       <img
         src={pokemon.sprites.other["official-artwork"].front_default}
-        style={{ position: "absolute", height: "80%", right: "-3rem" }}
+        className="pokemonImage"
         alt="pokemon image"
       />
     </Link>
